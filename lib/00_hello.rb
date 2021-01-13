9b944da0d79d50871d83ea1cb5640cb38dfc544c
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-13 13:31:23 +0100

    Update

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,5 +1,12 @@
-def say_hello
-  puts "Enter name: "
-  name = gets.chomp
-  return name
+def ask_first_name
+  puts "Enter first name:"
+  first_name = gets.chomp
+  return first_name
 end
+
+def say_hello
+  name = ask_first_name
+  return "Hello #{name} :)"
+end
+
+puts say_hello
