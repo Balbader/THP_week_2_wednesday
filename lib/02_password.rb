722b60c886c01c9f6ba8ab9d193004e95d5b9287
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-13 18:04:24 +0100

    Update

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -10,15 +10,18 @@ def login
   puts "Enter password: "
   print ">"
   new_pwd = gets.chomp
-  if new_pwd != pwd 
+  if new_pwd == pwd
+    welcome_screen
+  elsif new_pwd != pwd 
     while new_pwd != pwd
     puts "Wrong password!"
     puts "Please try again: "
     print "> "
     new_pwd = gets.chomp
     end
-  else
-    welcome_screen
+    if new_pwd == pwd
+      welcome_screen
+    end
   end
 end
 
@@ -26,9 +29,5 @@ def welcome_screen
   puts "Well done :)"
 end
 
-def perform
-  login
-  welcome_screen
-end
+login
 
-perform
