3655937828a755833c5a0495471da064c87a6012
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-13 20:20:29 +0100

    update

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,87 +1,33 @@
-def random
-  rand(1..6).to_i
-end
-
-def throw_dice
-  puts "Hit the letter 'd' on your keyboard to throw the dice followed by enter:"
-  init = gets.chomp
-  nbr = random
-  if init == "d"
-    nbr
-  end
-  return nbr
-end
-
-def play
-  hit = 10
-  stair = 0
-  while stair != hit
-    puts "Throw again:"
-    if throw_dice == 5 || throw_dice == 6
-      stair += 1
-      puts stair.to_s
-    end
-    if throw_dice == 1
-      stair -= 1
-      puts stair.to_s
-    end
-    if
-      stair == stair
-      puts stair.to_s
-    end
-
-    if stair == hit
-      puts "You Win"
+puts "Hit the letter 'd' on your keyboard followed by enter."
+answer = gets.chomp
+if answer == "d"
+  stairs = 0
+  nbr = rand(1..6).to_i
+  while stairs < 10
+    nbr = rand(1..6).to_i
+    if stairs < 10
+      if nbr == 5 || nbr == 6
+        stairs += 1
+        puts stairs.to_s
+        puts "Hit the letter 'd' on your keyboard followed by enter."
+        answer = gets.chomp
+      end
+      if nbr == 1
+        stairs -= 1
+        puts stairs.to_s
+        puts "Hit the letter 'd' on your keyboard followed by enter."
+        answer = gets.chomp
+      end
+      if nbr == 2 || nbr == 3 || nbr == 4
+        stairs == stairs
+        puts stairs.to_s
+        puts "Hit the letter 'd' on your keyboard followed by enter."
+        answer = gets.chomp
+      end
+      if stairs == 10
+        puts "You Win"
+      end
     end
   end
 end
 
-play
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-.compteur du nombre de lancé de des
-.lancer le des
-.initiate random number
-.5/6 === + 1
-.1 === -1
-.2/3/4 === 0
-10 === win!
-
-def count
-  count = 0
-  if 
-    count += 1
-  end
-end
-=end
